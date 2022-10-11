1b88521118fc36363171711b0154c660beae4547
a.avdiuk <a.avdiuk@eurokappa.ru>
2022-10-11 15:33:16 +0300

    1 component

---
+++ local/components/prognos9ys/header.user/class.php
@@ -0,0 +1,10 @@
+<?php
+
+class Header_user extends CBitrixComponent {
+
+    public function __construct($component = null)
+    {
+        parent::__construct($component);
+    }
+
+}
--- local/templates/prognos9s/header.php
+++ local/templates/prognos9s/header.php
@@ -1,16 +1,39 @@
 <?
-if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();?>
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die(); ?>
 <!DOCTYPE html>
 <html>
-	<head>
-		<?$APPLICATION->ShowHead();?>
-		<title><?$APPLICATION->ShowTitle();?></title>
-		<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" /> 	
-	</head>
-	<body>
-		<div id="panel">
-			<?$APPLICATION->ShowPanel();?>
-		</div>
+<head>
+    <? $APPLICATION->ShowHead(); ?>
+    <title><? $APPLICATION->ShowTitle(); ?></title>
+    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico"/>
+</head>
+<body>
+<div id="panel">
+    <? $APPLICATION->ShowPanel(); ?>
+</div>
+
+<?php $APPLICATION->IncludeComponent(
+    "prognos9ys:header.user",
+    "",
+    $component,
+    false
+);
+?>
+
+<div class="container">
+    <p><?
+        echo '<pre>';
+        $userId = CUser::GetID();
+        $arUser = CUser::GetByID($userId)->GetNext();
+
+        //            var_dump(CUser::GetList());
+        echo '</pre>';
+
+        ?>
+        pre
+    </p>
+<!--    <img src="--><?//= CFile::GetPath($arUser['PERSONAL_PHOTO']) ?><!--" alt="">-->
+</div>
 
 
 	
