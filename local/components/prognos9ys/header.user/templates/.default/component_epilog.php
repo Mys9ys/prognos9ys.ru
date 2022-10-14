cd5c908ef5114a37956136e8970a98034a0195ab
a.avdiuk <a.avdiuk@eurokappa.ru>
2022-10-14 17:28:52 +0300

    bootstrap icon

--- local/components/prognos9ys/header.user/templates/.default/component_epilog.php
+++ local/components/prognos9ys/header.user/templates/.default/component_epilog.php
@@ -4,7 +4,7 @@ if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
 use Bitrix\Main\Page\Asset;
 
 // Для подключения css
-Asset::getInstance()->addCss(SITE_TEMPLATE_PATH . "/style.min.css");
+Asset::getInstance()->addCss($templateFolder . "/assets/style.min.css");
 
 // Для подключения скриптов
 //Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/js/myscripts.js");
--- local/components/prognos9ys/header.user/templates/.default/template.php
+++ local/components/prognos9ys/header.user/templates/.default/template.php
@@ -1,5 +1,8 @@
 <?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();?>
 <div class="user_profile">
     <div class="user_title"><?=$arResult['NAME']?></div>
-    <img src="<?=$arResult['img']?>" width="60" alt="">
+<!--    <img src="--><?//=$arResult['img']?><!--" width="60" alt="">-->
+    <i class="bi bi-8-circle" style="color: green"></i>
+    <a href="/" class="test_link">Main</a>
+    <div class="test_div"></div>
 </div>
