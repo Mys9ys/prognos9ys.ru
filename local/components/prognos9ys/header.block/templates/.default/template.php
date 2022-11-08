33bf539d2e2f91f909fa0ccfacea58000ee1b26f
a.avdiuk <a.avdiuk@eurokappa.ru>
2022-11-08 13:43:11 +0300

    хеадер

--- local/components/prognos9ys/header.block/templates/.default/template.php
+++ local/components/prognos9ys/header.block/templates/.default/template.php
@@ -1,13 +1,43 @@
 <?php if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();?>
 
-<div class="container header_block">
+<!--<div class="container header_block">-->
+<!--    <i class="bi bi-menu-up"></i>-->
+<!--    --><?php //$APPLICATION->IncludeComponent(
+//        "prognos9ys:header.user",
+//        "",
+//        array(),
+//        $component,
+//        array()
+//    ); ?>
+<!--</div>-->
 
-    <?php $APPLICATION->IncludeComponent(
-        "prognos9ys:header.user",
-        "",
-        array(),
-        $component,
-        array()
-    ); ?>
+
+<div class="container header_wrapper">
+    <div class="h_main_block">
+        <div class="hm_left_block">
+            <div class="hm_achieve_block">
+                <div class="hm_achieve_box hm_box">
+                    <i class="bi bi-disc"></i>
+                </div>
+                <div class="hm_rank_box hm_box">Rank 2</div>
+            </div>
+            <div class="hm_btn_block">
+                <i class="bi bi-menu-up"></i>
+                <a href="/p/match/"><i class="bi bi-pencil-square"></i></a>
+            </div>
+        </div>
+        <div class="hm_ava_block">
+            <img class="hm_ava_img" src="<?=$templateFolder?> /assets/img/ava_no.jpg" alt="">
+        </div>
+        <div class="hm_right_block">
+            <div class="hm_nick_box hm_box">
+                Mgsdssss Ateerrre
+            </div>
+            <div class="hm_btn_block">
+                <i class="bi bi-cup"></i>
+                <i class="bi bi-door-open"></i>
+            </div>
+        </div>
+    </div>
 
 </div>
