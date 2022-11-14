caf94d1768217b066475499b6b6102c7990b299a
a.avdiuk <a.avdiuk@eurokappa.ru>
2022-11-14 10:48:03 +0300

    favicon

--- local/templates/prognos9ys/header.php
+++ local/templates/prognos9ys/header.php
@@ -4,7 +4,10 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die(); ?>
 <html>
 <head>
     <title><? $APPLICATION->ShowTitle(); ?></title>
-    <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico"/>
+    <link rel="apple-touch-icon" sizes="180x180" href="/local/templates/prognos9ys/assets/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/local/templates/prognos9ys/assets/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/local/templates/prognos9ys/assets/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/local/templates/prognos9ys/assets/favicon/site.webmanifest">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <?
