367233ca7d4b5f5e232f85277e4a79b7363f8ddf
a.avdiuk <a.avdiuk@eurokappa.ru>
2022-11-24 15:42:51 +0300

    изменение расписания

--- local/components/prognos9ys/football.matches/templates/.default/result_modifier.php
+++ local/components/prognos9ys/football.matches/templates/.default/result_modifier.php
@@ -1,4 +1,21 @@
 <?php
 if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true)die();
 
-
+$arResult["future"] = [
+    ['date' => '03.12', 'time' => '18:00', 'number'=>'A01', 'home'=>'A1', 'guest'=>'B2'],
+    ['date' => '03.12', 'time' => '22:00', 'number'=>'A02', 'home'=>'C1', 'guest'=>'D2'],
+    ['date' => '04.12', 'time' => '18:00', 'number'=>'A03', 'home'=>'D1', 'guest'=>'C2'],
+    ['date' => '04.12', 'time' => '22:00', 'number'=>'A04', 'home'=>'B1', 'guest'=>'A2'],
+    ['date' => '05.12', 'time' => '18:00', 'number'=>'A05', 'home'=>'E1', 'guest'=>'F2'],
+    ['date' => '05.12', 'time' => '22:00', 'number'=>'A06', 'home'=>'G1', 'guest'=>'H2'],
+    ['date' => '06.12', 'time' => '18:00', 'number'=>'A07', 'home'=>'F1', 'guest'=>'E2'],
+    ['date' => '06.12', 'time' => '22:00', 'number'=>'A08', 'home'=>'H1', 'guest'=>'G2'],
+    ['date' => '09.12', 'time' => '18:00', 'number'=>'QF1', 'home'=>'A05', 'guest'=>'A06'],
+    ['date' => '09.12', 'time' => '22:00', 'number'=>'QF2', 'home'=>'A01', 'guest'=>'A02'],
+    ['date' => '10.12', 'time' => '18:00', 'number'=>'QF3', 'home'=>'A07', 'guest'=>'A08'],
+    ['date' => '10.12', 'time' => '22:00', 'number'=>'QF4', 'home'=>'A03', 'guest'=>'A04'],
+    ['date' => '13.12', 'time' => '22:00', 'number'=>'SF1', 'home'=>'WQF1', 'guest'=>'WQF2'],
+    ['date' => '14.12', 'time' => '22:00', 'number'=>'SF2', 'home'=>'WQF3', 'guest'=>'WQF4'],
+    ['date' => '17.12', 'time' => '18:00', 'number'=>'F3', 'home'=>'LSF1', 'guest'=>'LSF2'],
+    ['date' => '18.12', 'time' => '18:00', 'number'=>'F1', 'home'=>'WSF1', 'guest'=>'WSF2'],
+];
--- local/components/prognos9ys/football.matches/templates/.default/template.php
+++ local/components/prognos9ys/football.matches/templates/.default/template.php
@@ -8,68 +8,209 @@
             Расписание матчей Чемпионата мира по футболу
         </div>
     </div>
-    <?php $day = '';
-    foreach ($arResult["teams"] as $id=>$item):?>
-    <?php
-    if($day !== $item["date"]):?>
-        <div class="day_line_block">
-            <div class="day_line_box">
-                <div class="day_date m_template_box"><i class="bi bi-calendar4-event"></i> <?=$item["date"]?></div>
+    <?php $day = '';?>
+
+    <div class="accordion" id="accordionExample">
+        <div class="accordion-item">
+            <h6 class="accordion-header" id="headingOne">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapseOne"
+                        aria-expanded="false" aria-controls="collapseOne">
+                   Прошедшие
+                </button>
+            </h6>
+            <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne"
+                 data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    <?foreach ($arResult["teams"] as $id=>$item):?>
+                    <?php if($item["active"] === 'Y') continue;?>
+                        <?php
+                        if($day !== $item["date"]):?>
+                            <div class="day_line_block">
+                                <div class="day_line_box">
+                                    <div class="day_date m_template_box"><i class="bi bi-calendar4-event"></i> <?=$item["date"]?></div>
+                                </div>
+                            </div>
+                        <?endif;
+                        $day = $item["date"];
+                        ?>
+                        <div class="m_match_wrapper">
+                            <div class="m_match_box">
+                                <div class="m_number">#<?=$item["number"]?></div>
+                                <div class="m_time m_template_box"><i class="bi bi-alarm"></i> <?=$item["time"]?></div>
+                                <div class="m_team_block m_template_box">
+                                    <div class="m_home_team_box m_team_box">
+                                        <div class="m_team_flag">
+                                            <img class="mt_flag_img" src="<?=$item["home"]["img"]?>" alt="">
+                                        </div>
+                                        <div class="m_team_title"><?=$item["home"]["NAME"]?></div>
+                                    </div>
+                                    <div class="m_separate">-</div>
+                                    <div class="m_guest_team_box m_team_box">
+                                        <div class="m_team_flag">
+                                            <img class="mt_flag_img" src="<?=$item["guest"]["img"]?>" alt="">
+                                        </div>
+                                        <div class="m_team_title"><?=$item["guest"]["NAME"]?></div>
+                                    </div>
+                                </div>
+                                <div class="m_goals_box m_template_box">
+                                    <div class="m_goals mg_home"><?=$item["home"]["goals"]?></div>
+                                    <div class="m_separate">-</div>
+                                    <div class="m_goals mg_guest"><?=$item["guest"]["goals"]?></div>
+                                </div>
+                            </div>
+                            <div class="match_all_info_block">
+                                <div class="match_user_info">
+                                    <?php if($item["write"]):?>
+                                        <span class="text-success">Вы заполнили: <?=$item["write"]?></span>
+                                    <?php else:?>
+                                        <span class="text-info">Не заполнено</span>
+                                    <?php endif;?>
+                                </div>
+                                <a class="match_write_btn" href="/p/match/<?=$id?>/">
+                                    <?php if($item["active"] === 'Y'):?>
+                                        <?php if($item["write"]):?>
+                                            Изменить <i class="bi bi-pencil-square"></i>
+                                        <?php else:?>
+                                            Заполнить <i class="bi bi-pencil-square"></i>
+                                        <?php endif;?>
+                                    <?php else:?>
+                                        Проверить </i>
+                                    <?php endif;?>
+                                </a>
+                            </div>
+                        </div>
+                    <?php endforeach;?>
+                </div>
             </div>
         </div>
-    <?endif;
-        $day = $item["date"];
-    ?>
-    <div class="m_match_wrapper">
-        <div class="m_match_box">
-            <div class="m_number">#<?=$item["number"]?></div>
-            <div class="m_time m_template_box"><i class="bi bi-alarm"></i> <?=$item["time"]?></div>
-            <div class="m_team_block m_template_box">
-                <div class="m_home_team_box m_team_box">
-                    <div class="m_team_flag">
-                        <img class="mt_flag_img" src="<?=$item["home"]["img"]?>" alt="">
-                    </div>
-                    <div class="m_team_title"><?=$item["home"]["NAME"]?></div>
-                </div>
-                <div class="m_separate">-</div>
-                <div class="m_guest_team_box m_team_box">
-                    <div class="m_team_flag">
-                        <img class="mt_flag_img" src="<?=$item["guest"]["img"]?>" alt="">
-                    </div>
-                    <div class="m_team_title"><?=$item["guest"]["NAME"]?></div>
+        <div class="accordion-item">
+            <h6 class="accordion-header" id="headingTwo">
+                <button class="accordion-button" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                    Текущие
+                </button>
+            </h6>
+            <div id="collapseTwo" class="accordion-collapse collapse show" aria-labelledby="headingTwo"
+                 data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    <?foreach ($arResult["teams"] as $id=>$item):?>
+                        <?php if($item["active"] === 'N') continue;?>
+                        <?php
+                        if($day !== $item["date"]):?>
+                            <div class="day_line_block">
+                                <div class="day_line_box">
+                                    <div class="day_date m_template_box"><i class="bi bi-calendar4-event"></i> <?=$item["date"]?></div>
+                                </div>
+                            </div>
+                        <?endif;
+                        $day = $item["date"];
+                        ?>
+                        <div class="m_match_wrapper">
+                            <div class="m_match_box">
+                                <div class="m_number">#<?=$item["number"]?></div>
+                                <div class="m_time m_template_box"><i class="bi bi-alarm"></i> <?=$item["time"]?></div>
+                                <div class="m_team_block m_template_box">
+                                    <div class="m_home_team_box m_team_box">
+                                        <div class="m_team_flag">
+                                            <img class="mt_flag_img" src="<?=$item["home"]["img"]?>" alt="">
+                                        </div>
+                                        <div class="m_team_title"><?=$item["home"]["NAME"]?></div>
+                                    </div>
+                                    <div class="m_separate">-</div>
+                                    <div class="m_guest_team_box m_team_box">
+                                        <div class="m_team_flag">
+                                            <img class="mt_flag_img" src="<?=$item["guest"]["img"]?>" alt="">
+                                        </div>
+                                        <div class="m_team_title"><?=$item["guest"]["NAME"]?></div>
+                                    </div>
+                                </div>
+                                <div class="m_goals_box m_template_box">
+                                    <div class="m_goals mg_home"><?=$item["home"]["goals"]?></div>
+                                    <div class="m_separate">-</div>
+                                    <div class="m_goals mg_guest"><?=$item["guest"]["goals"]?></div>
+                                </div>
+                            </div>
+                            <div class="match_all_info_block">
+                                <div class="match_user_info">
+                                    <?php if($item["write"]):?>
+                                        <span class="text-success">Вы заполнили: <?=$item["write"]?></span>
+                                    <?php else:?>
+                                        <span class="text-info">Не заполнено</span>
+                                    <?php endif;?>
+                                </div>
+                                <a class="match_write_btn" href="/p/match/<?=$id?>/">
+                                    <?php if($item["active"] === 'Y'):?>
+                                        <?php if($item["write"]):?>
+                                            Изменить <i class="bi bi-pencil-square"></i>
+                                        <?php else:?>
+                                            Заполнить <i class="bi bi-pencil-square"></i>
+                                        <?php endif;?>
+                                    <?php else:?>
+                                        Проверить </i>
+                                    <?php endif;?>
+                                </a>
+                            </div>
+                        </div>
+                    <?php endforeach;?>
                 </div>
             </div>
-            <div class="m_goals_box m_template_box">
-                <div class="m_goals mg_home"><?=$item["home"]["goals"]?></div>
-                <div class="m_separate">-</div>
-                <div class="m_goals mg_guest"><?=$item["guest"]["goals"]?></div>
-            </div>
-            <!--        <div class="m_info_box m_template_box">-->
-            <!--            <a class="m_btn_prognosis" href="/p/match/--><?//=$id?><!--/"><i class="bi bi-pencil-square"></i></a>-->
-            <!--        </div>-->
         </div>
-        <div class="match_all_info_block">
-            <div class="match_user_info">
-                <?php if($item["write"]):?>
-                <span class="text-success">Вы заполнили: <?=$item["write"]?></span>
-                <?php else:?>
-                <span class="text-info">Не заполнено</span>
-                <?php endif;?>
+        <div class="accordion-item">
+            <h6 class="accordion-header" id="headingThree">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                    Будущие
+                </button>
+            </h6>
+            <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree"
+                 data-bs-parent="#accordionExample">
+                <div class="accordion-body">
+                    <?foreach ($arResult["future"] as $id=>$item):?>
+                        <?php
+                        if($day !== $item["date"]):?>
+                            <div class="day_line_block">
+                                <div class="day_line_box">
+                                    <div class="day_date m_template_box"><i class="bi bi-calendar4-event"></i> <?=$item["date"]?></div>
+                                </div>
+                            </div>
+                        <?endif;
+                        $day = $item["date"];
+                        ?>
+                        <div class="m_match_wrapper">
+                            <div class="m_match_box">
+                                <div class="m_number"><?=$item["number"]?></div>
+                                <div class="m_time m_template_box"><i class="bi bi-alarm"></i> <?=$item["time"]?></div>
+                                <div class="m_team_block m_template_box">
+                                    <div class="m_home_team_box m_team_box">
+                                        <div class="m_team_title"><?=$item["home"]?></div>
+                                    </div>
+                                    <div class="m_separate">-</div>
+                                    <div class="m_guest_team_box m_team_box">
+                                        <div class="m_team_title"><?=$item["guest"]?></div>
+                                    </div>
+                                </div>
+                                <div class="m_goals_box m_template_box">
+                                    <div class="m_goals mg_home">0</div>
+                                    <div class="m_separate">-</div>
+                                    <div class="m_goals mg_guest">0</div>
+                                </div>
+                            </div>
+                            <div class="match_all_info_block">
+                                <div class="match_user_info">
+                                   Ожидается заполнение
+                                </div>
+                                <div class="match_write_btn" href="/p/match/<?=$id?>/">
+                                    Ожидайте
+                                </div>
+                            </div>
+                        </div>
+                    <?php endforeach;?>
+                </div>
             </div>
-            <a class="match_write_btn" href="/p/match/<?=$id?>/">
-                <?php if($item["active"] === 'Y'):?>
-                    <?php if($item["write"]):?>
-                        Изменить <i class="bi bi-pencil-square"></i>
-                    <?php else:?>
-                        Заполнить <i class="bi bi-pencil-square"></i>
-                    <?php endif;?>
-                <?php else:?>
-                    Проверить </i>
-                <?php endif;?>
-            </a>
         </div>
     </div>
-
-    <?php endforeach;?>
 </div>
+
+
 
