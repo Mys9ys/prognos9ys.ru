8bd82a24808f1ec131f1d5d70710dfa2a938024e
a.avdiuk <a.avdiuk@eurokappa.ru>
2022-12-02 10:16:54 +0300

    убраны ничьи из популярных

--- local/components/prognos9ys/football.one.match/templates/.default/result_modifier.php
+++ local/components/prognos9ys/football.one.match/templates/.default/result_modifier.php
@@ -4,9 +4,9 @@ if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED!==true)die();
 $arResult["btn"]["goals"]["score"] = [
    ["name"=>"3-0", "cell"=> "goal"],
    ["name"=>"2-1", "cell"=> "goal"],
-   ["name"=>"1-1", "cell"=> "goal"],
+   ["name"=>"2-0", "cell"=> "goal"],
    ["name"=>"1-0", "cell"=> "goal"],
-   ["name"=>"0-0", "cell"=> "goal"],
+   ["name"=>"0-2", "cell"=> "goal"],
    ["name"=>"0-1", "cell"=> "goal"],
    ["name"=>"1-2", "cell"=> "goal"],
    ["name"=>"1-3", "cell"=> "goal"],
